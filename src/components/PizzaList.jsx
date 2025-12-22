05d687dec7cff30f645a2856312d38296e3dfc30
Gonzalo Vasque <gonzalovasquez@live.cl>
2025-12-21 23:41:35 -0300

    nuevos cambios

--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -1,17 +1,36 @@
-import { pizzas } from "./pizzas";
-import CardPizza from "./CardPizza";
+import { useEffect, useState } from "react";
+import Pizza from "./Pizza";
 
 const PizzaList = ({ pizzaCart, setPizzaCart }) => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    const getPizzas = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/pizzas");
+        const data = await res.json();
+        setPizzas(data);
+      } catch (error) {
+        console.error("Error al obtener pizzas", error);
+      }
+    };
+
+    getPizzas();
+  }, []);
+
   return (
-    <div className="pizza-grid">
-      {pizzas.map((pizza) => (
-        <CardPizza
-          key={pizza.id}
-          pizza={pizza}
-          pizzaCart={pizzaCart}
-          setPizzaCart={setPizzaCart}
-        />
-      ))}
+    <div className="container mt-4">
+      <div className="row">
+        {pizzas.map((pizza) => (
+          <Pizza
+            key={pizza.id}
+            pizza={pizza}
+            pizzaCart={pizzaCart}
+            setPizzaCart={setPizzaCart}
+          />
+
+        ))}
+      </div>
     </div>
   );
 };
